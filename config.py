3ce0c69c478ff8e7c6897cb1782f4e73555ada90
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-17 22:47:43 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

---
+++ config.py
@@ -0,0 +1,41 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", 27353035))
+API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+OWNER_ID = int(getenv("OWNER_ID", 6919772024))
+MONGO_URL = getenv("MONGO_URL", None)
+SUPPORT_GRP = getenv("SUPPORT_GRP", "TG_NAME_STYLE")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "TKS_JOIN")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "chin_tabak_dum_dum")
+
+# Random Start Images
+IMG = [
+    "https://telegra.ph/file/870bbc6b040bf757b849c.png",
+]
+
+
+# Random Stickers
+STICKER = [
+    "CAACAgQAAxkBAALRi2NZXUgjZCT775L5Nr0XrLbQ6XIpAAK_EQACpvFxHq2xh5JRVJNrKgQ",
+    "CAACAgQAAxkBAALRjGNZXUs6YPggISBdtg4nXaU0vjNzAALqCwACbCIRU61ZQKi3F88DKgQ",
+    "CAACAgQAAxkBAALRjWNZXUvETcfHR2Yi9ftTQLLP2uD8AAIVDAAC1SMQU-QrCHEcbz8rKgQ",
+]
+
+
+EMOJIOS = [
+    "💣",
+    "💥",
+    "🪄",
+    "🧨",
+    "⚡",
+    "🤡",
+    "👻",
+    "🎃",
+    "🎩",
+    "🕊",
+]
