3d7a0f907e8f406a15d4a3ae877d84d69d973a92
˹Ҩ፝֟፝ɴ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡゙ <laleshwarraj018@gmail.com>
2024-10-17 22:57:20 +0530

    ♡︎ ˹Ҩ፝֟፝ͷ ꫝɴᴊᴀʟɪ˼ [🇮🇳] ♡

---
+++ Mickey/modules/start.py
@@ -0,0 +1,90 @@
+# Don't remove This Line From Here. Tg: @Dev_Arora_0981 | @DevArora0981
+# Github :- Devarora-0981 | Devarora2604
+
+import asyncio
+import random
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import EMOJIOS, IMG, STICKER
+from Mickey import MickeyBot
+from Mickey.database.chats import add_served_chat
+from Mickey.database.users import add_served_user
+from Mickey.modules.helpers import (
+    CLOSE_BTN,
+    DEV_OP,
+    HELP_BTN,
+    HELP_BUTN,
+    HELP_READ,
+    HELP_START,
+    SOURCE_READ,
+    START,
+)
+
+
+@MickeyBot.on_cmd(["statsert", "starserft"])
+async def start(_, m: Message):
+    if m.chat.type == ChatType.PRIVATE:
+        accha = await m.reply_text(
+            text=random.choice(EMOJIOS),
+        )
+        await asyncio.sleep(1.3)
+        await accha.edit("__ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ꨄ︎ ᴡᴀɪᴛ..🥵__)
+        await asyncio.sleep(0.2)
+        await accha.edit("__ʜᴇʏ ʙᴀʙʏ ꨄ ʜᴏᴡ ᴀʀᴇ ʏᴏᴜ⚡.....__")
+        await asyncio.sleep(0.2)
+        await accha.edit("__ʙᴏᴛ sᴛᴀʀᴛɪɴɢ ꨄ︎ ʙᴀʙʏ📍..__")
+        await asyncio.sleep(0.2)
+        await accha.delete()
+        umm = await m.reply_sticker(sticker=random.choice(STICKER))
+        await asyncio.sleep(2)
+        await umm.delete()
+        await m.reply_photo(
+            photo=random.choice(IMG),
+            caption=f"""**🥀💨ɢʀᴏᴜᴘ ᴊᴏɪɴ ᴋʀ ʟᴏ 🌷°‌⁪༎꯭ࠫ𝆺𝅥•๋⸙‌ٖٖٖٖٖٖٜٖٖ [⚡ᴊᴏɪɴ⚡](https://t.me/ANJALIWORLD) 🍷
+ 🍷💨ᴍʏ ᴄᴜᴛᴇ ᴏᴡɴᴇʀ🍒🥀 [ᴍᴀʜᴛᴏ ✯ ᴀɴᴊᴀʟɪ ](https://t.me/AnjaliOwnerBot) """,
+            reply_markup=InlineKeyboardMarkup(DEV_OP),
+        )
+        await add_served_user(m.from_user.id)
+    else:
+        await m.reply_photo(
+            photo=random.choice(IMG),
+            caption=START,
+            reply_markup=InlineKeyboardMarkup(HELP_START),
+        )
+        await add_served_chat(m.chat.id)
+
+
+@MickeyBot.on_cmd("hehrlp")
+async def help(client: MickeyBot, m: Message):
+    if m.chat.type == ChatType.PRIVATE:
+        hmm = await m.reply_photo(
+            photo=random.choice(IMG),
+            caption=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+        )
+        await add_served_user(m.from_user.id)
+    else:
+        await m.reply_photo(
+            photo=random.choice(IMG),
+            caption="**ʜᴇʏ, ᴘᴍ ᴍᴇ ғᴏʀ ʜᴇʟᴘ ᴄᴏᴍᴍᴀɴᴅs!**",
+            reply_markup=InlineKeyboardMarkup(HELP_BUTN),
+        )
+        await add_served_chat(m.chat.id)
+
+
+@MickeyBot.on_cmd("on")
+async def repo(_, m: Message):
+    await m.reply_text(
+        text=SOURCE_READ,
+        reply_markup=InlineKeyboardMarkup(CLOSE_BTN),
+        disable_web_page_preview=True,
+    )
+
+
+@MickeyBot.on_message(filters.new_chat_members)
+async def welcome(_, m: Message):
+    for member in m.new_chat_members:
+        await m.reply_photo(photo=random.choice(IMG), caption=START)
